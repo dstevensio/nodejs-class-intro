b167ae5a30d5a4b9607fad9bdff66000e835802a
Dave Stevens <dave@davestevens.us>
2013-06-18 13:51:54 -0700

    final step

--- app/app.js
+++ app/app.js
@@ -4,12 +4,23 @@ var app = express();
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'hbs');
 
+app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   var content = {
     message: "Hello World!",
     markup: '<a href="#">OMG a link</a>'
+  };
+
+  res.render('boilerplate', content);
+});
+
+app.post('/', function (req, res) {
+  var content = {
+    message: "Say what?",
+    markup: '<a href="/">Return home</a>',
+    enteredText: req.body.enteredText
   };
 
   res.render('boilerplate', content);
