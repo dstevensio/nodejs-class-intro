97c1788dd5fe0e1a1cffd771774d0aba97fd4143
Dave Stevens <dave@davestevens.us>
2013-06-18 13:19:01 -0700

    up to step 4

--- app/app.js
+++ app/app.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'hbs');
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var content = {
+    message: "Hello World!",
+    markup: '<a href="#">OMG a link</a>'
+  };
+  
+  res.render('index', content);
 });
 
 app.listen(3000);
